4747f055403e3a00f1fe7e451816e5dc4d7c30e9
stolyarov_as <stolyarov_as@rtsoft.msk.ru>
2025-08-04 22:10:48 +0300

    pep

--- tests/test_parse_time.py
+++ tests/test_parse_time.py
@@ -5,9 +5,9 @@ from src.ui.components import BranchTimeEntry
 @pytest.mark.parametrize(
     'time, expected_time',
     [
-        ('0:0', (0,0)),
-        ('0:10', (0,10)),
-        ('1:0', (1,0)),
+        ('0:0', (0, 0)),
+        ('0:10', (0, 10)),
+        ('1:0', (1, 0)),
         ('1h10m', (1, 10)),
         ('1h 10m', (1, 10)),
         ('1h', (1, 0)),
